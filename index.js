bc299ef85b2e111afd9f9fdf24fec04afcdc3175
gabi91cata <gabi91cata@yahoo.com>
2015-05-14 13:40:01 +0300

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require("http");
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 5000;
-app.use(express.static(__dirname ));
+app.use(express.static(__dirname +"/"));
 var server = http.createServer(app);
 server.listen(port);
 var pool = mysql.createPool({
