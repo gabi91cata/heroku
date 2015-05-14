d23c755c212f19836f132cd618aaf8e9afcf4169
gabi91cata <gabi91cata@yahoo.com>
2015-05-14 13:30:57 +0300

    make it better

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require("http")
 var express = require("express")
 var app = express()
 var port = process.env.PORT || 5000
-var things = require("api.js");
+var things = require("./api.js");
 
 app.use(express.static(__dirname + "/api"))
 
